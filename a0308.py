e861fbb12d06666ba45fc55b83671672650e9608
Eva <Eva19921013@icloud.com>
2017-07-11 10:46:14 +0800

    定位多个元素

---
+++ a0308.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+from selenium import webdriver
+import os,time
+
+browser=webdriver.Chrome()
+browser.get('file:///'+os.path.abspath('1.html'))
+#inputs=browser.find_elements_by_tag_name('input')
+#for input in inputs:
+#	if input.get_attribute('type')=='radio':
+#		input.click()
+
+checkboxs=browser.find_elements_by_css_selector('input[type=checkbox]')
+for checkbox in checkboxs:
+	checkbox.click()
+
+print '勾选所有checkbox'
+time.sleep(10)
+
+browser.quit()
